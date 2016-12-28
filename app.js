0fbed29526587f02f36c13db51599f13760383c8
TJ Egan <twegan@us.ibm.com>
2016-12-28 16:51:02 -0500

    finish 13

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const sliderImages = document.querySelectorAll('.slide-in');
 
-function checkSlide(e) {
-  console.log(e);
-}
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
 
-window.addEventListener('scroll', checkSlide);
+function checkSlide(e) {
+  sliderImages.forEach(img => {
+    // Halfway through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - (img.height / 2);
+    // Bottom of the image
+    const imageBottom = img.offsetTop + img.height;
+    
+    const isHalfShown = slideInAt > img.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      img.classList.add('active');
+    } else {
+      img.classList.remove('active');
+    }
+  });
+};
+
+window.addEventListener('scroll', debounce(checkSlide));
